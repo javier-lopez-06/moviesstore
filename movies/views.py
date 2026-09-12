85095af36d1ed773b9c108f3d82cc1ab76ad3046
Javier Lopez <javylopez06@gmail.com>
2026-09-12 15:48:36 -0400

    Add report review feature

--- movies/views.py
+++ movies/views.py
@@ -16,7 +16,8 @@ def index(request):
 
 def show(request, id):
     movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    reviews = Review.objects.filter(movie=movie, reported=False)
+
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
@@ -61,5 +62,12 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id,
         user=request.user)
     review.delete()
+    return redirect('movies.show', id=id)
+
+@login_required
+def report_review(request, id, review_id):
+    review = Review.objects.get(id=review_id, movie_id=id)
+    review.reported = True
+    review.save()
     return redirect('movies.show', id=id)
 
